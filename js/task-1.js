120b9d5879e02621c6870bf234279fe71b78d070
AnnaHodunko <fatova29@gmail.com>
2024-03-21 12:47:27 +0200

    hw/2

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,12 @@
 'use strict'; 
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
-    if (totalPrice >= customerCredits) {
-        return "Insufficient funds!";
-    } else {
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    }
+    // if (totalPrice >= customerCredits) {
+    //     return "Insufficient funds!";
+    // } else {
+    //     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    // }
+    return (totalPrice >= customerCredits) ? "Insufficient funds!" : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 console.log(makeTransaction(5, 3000, 23000)); 
 console.log(makeTransaction(3, 1000, 15000)); 
